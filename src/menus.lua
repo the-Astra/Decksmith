546beea2e8bd36c80334bc78205e3ce7ac7cb88e
the-Astra <djs8743@rit.edu>
2026-09-22 15:31:35 -0400

    This doesn't need to be very long

--- src/menus.lua
+++ src/menus.lua
@@ -458,7 +458,7 @@ Decksmith.customize_menu {
                                             w = 2,
                                             h = 1,
                                             all_caps = false,
-                                            max_length = 60,
+                                            max_length = 20,
                                             ref_table = Decksmith.start_args,
                                             ref_value = 'ds_author',
                                             extended_corpus = true
